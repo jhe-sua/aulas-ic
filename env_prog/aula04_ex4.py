12cf65eeef4758885156a52f42a1ef053629d4a9
Angel <C3007803@fgv.edu.br>
2025-05-14 12:28:17 +0000

    corregindo exercicio 4

--- env_prog/aula04_ex4.py
+++ env_prog/aula04_ex4.py
@@ -1,12 +1,13 @@
-def abrir_csv(paht_arquivo):
+def abrir_csv(paht_arquivo, separador, comeco = 0, fim_exlusivo = None):
+    """Abrir arquivo csv e comverter cada linha em uma tupla, onde cada elemento é um elemento da tupla"""
     lista = []
     arquivo = open(f"{paht_arquivo}", "r")
 
     for i in arquivo:
-        lista.append(tuple(p.strip('"') for p in i.strip().split(",")))
-    lista_total = lista[5:]
-    print(lista_total[5])
+        lista.append(tuple(p.strip('"') for p in i.strip().split(f"{separador}")))
+    lista_total = lista[comeco:fim_exlusivo]
+    print(lista_total)
     
     
     
-abrir_csv("/home/fgv/code/env_prog/arc/dados.csv")
+abrir_csv("/home/fgv/code/env_prog/arc/dados.csv",",",comeco=5)
